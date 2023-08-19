672b2adf8c1a517d58f31e08070a6f15d9106b90
luanym123vip <camduyen20013@gmail.com>
2023-08-19 20:39:11 +0700

    React Hooks show list todo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './views/Nav';
+import { useState } from 'react';
+import './App.scss'
+//template + logic
+// JSX  
+const App = () => {
 
-function App() {
+  const [name, setName] = useState('Dien')
+  const [value, setValue] = useState('')
+  const [list, setList] = useState([
+    { id: 'todo1', title: 'Watching TV' },
+    { id: 'todo2', title: 'Learning ReactJS Hook' },
+    { id: 'todo3', title: 'Playing Game' },
+  ])
+  const handlechangleInput = (event) => {
+    let changeValue = event.target.value;
+    setValue(changeValue)
+  }
+  const handleButtonClickMe = () => {
+    let newtodo = { id: 'abc', title: value };
+    setList([...list, newtodo]);
+  }
   return (
     <div className="App">
       <header className="App-header">
+        <Nav />
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Hook
-        </a>
+        <h1> Hello world with React Hook and {name}</h1>
+        <input type='text' value={value} onChange={(event) => handlechangleInput(event)} />
+        <button type='button' onClick={() => handleButtonClickMe()}>Click me</button>
+        <div className='todo-container'>
+          {list && list.length > 0 &&
+            list.map((item, index) => {
+              return (
+                <li className='todo-child' key={item.id}>
+                  {item.title}
+                </li>
+              )
+
+            })
+          }
+
+
+        </div>
       </header>
     </div>
   );
---
+++ src/views/Nav.js
@@ -0,0 +1,13 @@
+import "./Nav.scss"
+import logotruong from "./logo.png"
+const Nav = () => {
+    return (
+        <div class="topnav">
+            <a class="active" href="#home">Home</a>
+            <a href="#news">News</a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a>
+        </div>
+    )
+}
+export default Nav;
